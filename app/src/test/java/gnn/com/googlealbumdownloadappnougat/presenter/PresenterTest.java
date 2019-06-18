4ccae7fa126f767751ae58c4e89e979181133768
gnn06 <gilles.orsini@gmail.com>
2019-06-18 22:35:31 +0200

    add no album unit test

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/presenter/PresenterTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/presenter/PresenterTest.java
@@ -43,6 +43,42 @@ public class PresenterTest {
         assertNull(resulRequirement);
     }
 
+    @Test
+    public void onSyncClick_LastPermissionMissing() {
+        MainActivity activity = Mockito.mock(MainActivity.class);
+        Presenter presenter = Mockito.spy(new Presenter(activity, activity));
+        AuthManager auth = Mockito.mock(AuthManager.class);
+        Whitebox.setInternalState(presenter, "album", "test");
+        Mockito.when(auth.isSignIn()).thenReturn(true);
+        Mockito.when(auth.hasGooglePhotoPermission()).thenReturn(true);
+        Mockito.when(auth.hasWritePermission()).thenReturn(false);
+
+        IView view = Mockito.mock(IView.class);
+        PermissionRequirement taskExeq  = new PermissionRequirement(null, null, auth) {
+            @Override
+            public boolean checkRequirement() {
+                return true;
+            }
+
+            @Override
+            public void askAsyncRequirement() {
+
+            }
+
+            @Override
+            public void exec() {
+                System.out.println("execute req3");
+            }
+        };
+        PermissionRequirement writeReq  = new WritePermission(taskExeq, auth);
+        PermissionRequirement photoReq  = new GooglePhotoAPI_Requirement(writeReq, view, auth);
+        PermissionRequirement firstReq  = new SignRquirement(photoReq, view, auth);
+
+        PermissionRequirement resulRequirement = firstReq.checkAndExec();
+        assertEquals(taskExeq, resulRequirement);
+        Mockito.verify(auth, Mockito.times(1)).requestWritePermission();
+    }
+
     private PermissionRequirement getPermissionRequirement(final AuthManager auth) {
         IView view = Mockito.mock(IView.class);
         PermissionRequirement taskExeq  = new PermissionRequirement(null, null, auth) {

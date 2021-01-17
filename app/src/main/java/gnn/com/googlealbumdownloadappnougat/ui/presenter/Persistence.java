142c193e9a43783fe77061c83a2a859a626719fc
Gilles ORSINI <gilles.orsini@gmail.com>
2021-01-17 19:56:08 +0100

    comment

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/Persistence.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/Persistence.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 /**
- * Default values are keeped from Presenter
+ * Default values are taken from Presenter
  */
 public class Persistence {
 

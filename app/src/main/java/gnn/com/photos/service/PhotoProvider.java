1b0a7beaa9a42261ea5db962c963aebdbbd0a4c2
Gilles ORSINI <gilles.orsini@gmail.com>
2021-02-22 15:42:14 +0100

    add getPhotosFromIds

--- app/src/main/java/gnn/com/photos/service/PhotoProvider.java
+++ app/src/main/java/gnn/com/photos/service/PhotoProvider.java
@@ -3,13 +3,17 @@ package gnn.com.photos.service;
 import android.util.Log;
 
 import com.google.android.gms.auth.GoogleAuthException;
+import com.google.api.client.util.ArrayMap;
 import com.google.photos.library.v1.PhotosLibraryClient;
 import com.google.photos.library.v1.internal.InternalPhotosLibraryClient;
+import com.google.photos.library.v1.proto.BatchGetMediaItemsResponse;
+import com.google.photos.library.v1.proto.MediaItemResult;
 import com.google.photos.types.proto.Album;
 import com.google.photos.types.proto.MediaItem;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import gnn.com.photos.model.Photo;
 import gnn.com.photos.sync.Synchronizer;
@@ -52,6 +56,21 @@ public abstract class PhotoProvider {
             }
         }
         return null;
+    }
+
+    ArrayList<Photo> getPhotosFromIds(ArrayList<String> ids) throws IOException, GoogleAuthException {
+        ArrayList<Photo> result = null;
+        BatchGetMediaItemsResponse response = client.batchGetMediaItems(ids);
+        for (MediaItemResult item : response.getMediaItemResultsList()) {
+            // code == 3 if id does not exist
+            if (item.getStatus().getCode() == 0) {
+                if (result == null) {
+                    result = new ArrayList<>();
+                }
+                result.add(new Photo(item.getMediaItem().getBaseUrl(), item.getMediaItem().getId()));
+            }
+        }
+        return result;
     }
 
     public PhotosLibraryClient getClient() throws IOException, GoogleAuthException {

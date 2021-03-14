599ea2ab37d0d53d6ad227c629fbf506e633f390
Gilles ORSINI <gilles.orsini@gmail.com>
2021-03-14 12:31:36 +0100

    MyWorker take Synchronizer argument from inputData and instanciate Synchronizer in doWork

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/MyWorker.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/MyWorker.java
@@ -7,19 +7,42 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import java.io.File;
+import java.io.IOException;
+
 import gnn.com.googlealbumdownloadappnougat.photos.SynchronizerAndroid;
+import gnn.com.photos.service.RemoteException;
 
 public class MyWorker extends Worker {
+
+    // given from Presenter
+    private final SynchronizerAndroid synchronizer =  null;
+
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
+
+    // impossible d'injcter un synchronizer via le constructeur car on ne contrôle
+    // pas l'appel du contructeur ; utiliser inputData
 
     @NonNull
     @Override
     public Result doWork() {
         Log.i("WORKER","doWork");
-//        SynchronizerAndroid synchronizer = new SynchronizerAndroid(this.getApplicationContext(), cacheFile, cacheMaxage, processFolder);
-//        synchronizer.syncRandom(albumName, destinationFolder, rename, quantity);
-        return Result.success();
+        String albumName = getInputData().getString("album");
+        File destinationFolder = new File(getInputData().getString("folderPath"));
+        String rename = getInputData().getString("rename");
+        int quantity = getInputData().getInt("quantity", -1);
+        try {
+            Context activity = getApplicationContext();
+            File cacheFile = null;
+            long cacheMaxAge = 0;
+            File processFolder = null;
+            new SynchronizerAndroid((MainActivity) activity, cacheFile, cacheMaxAge, processFolder);
+            synchronizer.syncRandom(albumName, destinationFolder, rename, quantity);
+            return Result.success();
+        } catch (IOException | RemoteException e) {
+            return Result.failure();
+        }
     }
 }

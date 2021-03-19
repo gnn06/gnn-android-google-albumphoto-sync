7d0df79e6f82f6d1c314541614c577df7ffdfd74
Gilles ORSINI <gilles.orsini@gmail.com>
2021-03-19 21:22:33 +0100

    whennew is working

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/MyWorkerBasicTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/MyWorkerBasicTest.java
@@ -13,16 +13,22 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.powermock.modules.junit4.legacy.PowerMockRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 
 import java.io.IOException;
+
+import gnn.com.googlealbumdownloadappnougat.photos.SynchronizerAndroid;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(MyWorker.class)
 public class MyWorkerBasicTest {
 
     @Mock
@@ -54,8 +60,23 @@ public class MyWorkerBasicTest {
     }
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         MyWorker myWorker = new MyWorker(context, parameters);
+
+        Data data = new Data.Builder()
+                .putString("cacheAbsolutePath", tmpFolder.newFile().getAbsolutePath())
+                .putLong("cacheMaxAge", -1)
+                .putString("processAbsolutePath", tmpFolder.newFolder().getAbsolutePath())
+
+                .putString("folderPath", tmpFolder.newFolder().getAbsolutePath())
+
+                .build();
+        when(myWorker.getInputData()).thenReturn(data);
+
+        SynchronizerAndroid mock = PowerMockito.mock(SynchronizerAndroid.class);
+
+        PowerMockito.whenNew(SynchronizerAndroid.class).withAnyArguments().thenReturn(mock);
+        myWorker.doWork();
     }
 
 }

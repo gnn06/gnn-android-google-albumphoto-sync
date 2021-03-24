146e6e331d053a67dcb8bf3ea60c0b9ddad01bc0
Gilles ORSINI <gilles.orsini@gmail.com>
2021-03-24 10:45:20 +0100

    add logging using java.logging, create getFilename to mock more easily

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/MyWorker.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/MyWorker.java
@@ -9,12 +9,18 @@ import androidx.work.WorkerParameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import gnn.com.googlealbumdownloadappnougat.photos.SynchronizerAndroid;
 import gnn.com.photos.service.RemoteException;
 import gnn.com.photos.sync.Synchronizer;
 
 public class MyWorker extends Worker {
+
+    private final static String TAG = "MyWorker";
+    private final static String LOG_FILENAME = "MyWorker";
 
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -23,8 +29,6 @@ public class MyWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        Log.i("WORKER","doWork");
-
         File cacheFile = new File(getInputData().getString("cacheAbsolutePath"));
         long cacheMaxAge = getInputData().getLong("cacheMaxAge", -1);
         File processFolder = new File(getInputData().getString("processAbsolutePath"));
@@ -38,10 +42,29 @@ public class MyWorker extends Worker {
         int quantity = getInputData().getInt("quantity", -1);
         try {
             synchronizer.syncRandom(albumName, destinationFolder, rename, quantity);
+            getLogger().info("success");
+            Log.i(TAG, "success");
             return Result.success();
         } catch (IOException | RemoteException e) {
+            Log.e(TAG, e.getMessage());
             return Result.failure();
         }
+    }
+
+    private Logger getLogger() throws IOException {
+        Logger logger = Logger.getLogger(MyWorker.class.getName());
+        if (logger.getHandlers().length == 0) {
+            FileHandler fileHandler = new FileHandler(getFilename(), true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            // keep parent handler
+            logger.addHandler(fileHandler);
+            Log.i(TAG, "logger initialized");
+        }
+        return logger;
+    }
+
+    private String getFilename() {
+        return getApplicationContext().getFilesDir().getAbsolutePath() + LOG_FILENAME;
     }
 
 }

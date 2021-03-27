48aaab426d7d037d4b3b71bcb0b172287edeac5a
Gilles ORSINI <gilles.orsini@gmail.com>
2021-03-27 07:43:42 +0100

    code quality

--- app/src/androidTest/java/gnn/com/googlealbumdownloadappnougat/MyWorkerTest.java
+++ app/src/androidTest/java/gnn/com/googlealbumdownloadappnougat/MyWorkerTest.java
@@ -3,46 +3,32 @@ package gnn.com.googlealbumdownloadappnougat;
 import android.content.Context;
 import android.util.Log;
 
-import androidx.test.InstrumentationRegistry;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 import androidx.work.Configuration;
-import androidx.work.Data;
 import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.ListenableWorker;
-import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.testing.SynchronousExecutor;
-import androidx.work.testing.TestWorkerBuilder;
 import androidx.work.testing.WorkManagerTestInitHelper;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * not an real unit test
- * just usefull understand and check of WorkManagerTestInitializer works
+ * just useful understand and check of WorkManagerTestInitializer works
  *
  * in each test method, queue is empty
  */
@@ -50,7 +36,6 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class MyWorkerTest {
 
-    private  Context context;
     private ListenableFuture<List<WorkInfo>> info;
     private WorkManager workManager;
     private PeriodicWorkRequest request;
@@ -58,7 +43,7 @@ public class MyWorkerTest {
 
     @Before
     public void setUp() {
-        context = ApplicationProvider.getApplicationContext();
+        Context context = ApplicationProvider.getApplicationContext();
         Configuration config = new Configuration.Builder()
                 .setMinimumLoggingLevel(Log.DEBUG)
                 .setExecutor(new SynchronousExecutor())

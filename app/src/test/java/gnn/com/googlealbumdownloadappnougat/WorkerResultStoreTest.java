65709ddb6e99503dcb81628367b4ebfd16b94ded
Gilles ORSINI <gilles.orsini@gmail.com>
2021-04-07 22:12:29 +0200

    fix TU mocking context

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/WorkerResultStoreTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/WorkerResultStoreTest.java
@@ -1,22 +1,37 @@
 package gnn.com.googlealbumdownloadappnougat;
 
+import android.content.Context;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import androidx.work.Data;
 import androidx.work.WorkInfo;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
+@RunWith(PowerMockRunner.class)
 public class WorkerResultStoreTest {
 
-    WorkerResultStore store = new WorkerResultStore();
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @Test
     public void store_with_empty() throws IOException {
+        Context mock = mock(Context.class);
+        when(mock.getCacheDir()).thenReturn(tmpFolder.newFile());
+        WorkerResultStore store = new WorkerResultStore(mock);
 //        WorkInfo info = new WorkInfo(
 //                new UUID(123, 456),
 //                WorkInfo.State.ENQUEUED,

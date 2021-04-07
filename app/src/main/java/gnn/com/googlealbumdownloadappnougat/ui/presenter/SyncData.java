1f1f6452e12068df676cbca21e2ff4986d6e0733
Gilles ORSINI <gilles.orsini@gmail.com>
2021-04-07 21:33:46 +0200

    create SyncData

---
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/SyncData.java
@@ -0,0 +1,41 @@
+package gnn.com.googlealbumdownloadappnougat.ui.presenter;
+
+public class SyncData {
+
+    private String album;
+    private int quantity;
+    private String rename;
+    private String folderHuman;
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getRename() {
+        return rename;
+    }
+
+    public void setRename(String rename) {
+        this.rename = rename;
+    }
+
+    public String getFolderHuman() {
+        return folderHuman;
+    }
+
+    public void setFolderHuman(String folderHuman) {
+        this.folderHuman = folderHuman;
+    }
+}

606882ab4fae8819f7824930910072080909ce43
Gilles ORSINI <gilles.orsini@gmail.com>
2021-08-07 06:08:11 +0200

    add getLogger to WalpaperWorker + UT

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/wallpaper/WallPaperWorker.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wallpaper/WallPaperWorker.java
@@ -11,6 +11,12 @@ import androidx.work.WorkerParameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import gnn.com.googlealbumdownloadappnougat.photos.SynchronizerAndroid;
 import gnn.com.photos.service.RemoteException;
@@ -27,11 +33,28 @@ public class WallPaperWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Logger logger = getLogger();
         String destinationPath = getInputData().getString("folderPath");;
         File destinationFolder = getDestinationFolder(destinationPath);
         PhotoWallPaper photoWallPaper = new PhotoWallPaper((Activity)getApplicationContext(), destinationFolder);
         photoWallPaper.setWallpaper();
         return Result.success();
+    }
+
+    Logger getLogger() {
+        Logger logger = Logger.getLogger(this.getClass().getName());
+        Handler[] handlers = logger.getHandlers();
+        if (handlers.length == 0) {
+            try {
+                Handler handler = new FileHandler("/tmp/dev/toto.log");
+                Formatter formatter = new SimpleFormatter();
+                handler.setFormatter(formatter);
+                logger.addHandler(handler);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+        return logger;
     }
 
     private File getDestinationFolder(String album) {

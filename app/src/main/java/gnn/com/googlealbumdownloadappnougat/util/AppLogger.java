2193ca7d2beff8b70e5844814b32b0a226b666ef
Gilles ORSINI <gilles.orsini@gmail.com>
2021-10-09 22:03:32 +0200

    change log level to finest

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/util/AppLogger.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/util/AppLogger.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 import android.content.Context;
@@ -21,11 +22,13 @@ public class AppLogger {
 
     private static Logger getLogger(String path) {
         Logger logger = Logger.getLogger("worker");
+        logger.setLevel(Level.FINEST);
         Handler[] handlers = logger.getHandlers();
         if (handlers.length == 0) {
             try {
 //                Handler handler = new FileHandler("/tmp/dev/toto.log");
                 Handler handler = new FileHandler(path + "/app.log", true);
+                handler.setLevel(Level.FINEST);
                 Formatter formatter = new SimpleFormatter();
                 handler.setFormatter(formatter);
                 logger.addHandler(handler);

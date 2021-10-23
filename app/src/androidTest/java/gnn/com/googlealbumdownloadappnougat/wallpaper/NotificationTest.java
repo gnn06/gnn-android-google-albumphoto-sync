7a4b73d40b6f7083d8fc7e4b898207817aa2ad8d
Gilles ORSINI <gilles.orsini@gmail.com>
2021-10-23 09:20:51 +0200

    changement icon + UT tentative

---
+++ app/src/androidTest/java/gnn/com/googlealbumdownloadappnougat/wallpaper/NotificationTest.java
@@ -0,0 +1,20 @@
+package gnn.com.googlealbumdownloadappnougat.wallpaper;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+
+public class NotificationTest {
+
+    @Test
+    public void name() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Notification notification = new Notification(context);
+        notification.createNotificationChannel();
+        notification.show("message");
+    }
+}

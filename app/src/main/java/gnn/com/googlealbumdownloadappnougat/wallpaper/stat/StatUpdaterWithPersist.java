3320c9b3b526a547ce0a4815cf06d9f8d0c6a103
Gilles ORSINI <gilles.orsini@gmail.com>
2022-02-05 19:41:00 +0100

    create StatUpdaterWithPersist

---
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wallpaper/stat/StatUpdaterWithPersist.java
@@ -0,0 +1,28 @@
+package gnn.com.googlealbumdownloadappnougat.wallpaper.stat;
+
+import java.io.File;
+
+import gnn.com.googlealbumdownloadappnougat.util.DateProvider;
+
+class StatUpdaterWithPersist {
+
+    // Injected
+    private final File processFolder;
+
+    StatUpdaterWithPersist(File processFolder) {
+        this.processFolder = processFolder;
+    }
+
+    void  onWallpaperChange() {
+        PersistWallpaperStat persist = new PersistWallpaperStat(processFolder);
+        WallpaperStat stat;
+        try {
+            stat = persist.read();
+            new StatUpdater(new DateProvider()).updateOnNewChange(stat);
+            persist.write(stat);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}

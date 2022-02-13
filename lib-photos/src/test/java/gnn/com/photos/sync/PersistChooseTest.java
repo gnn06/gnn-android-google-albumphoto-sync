635b74745e9ea8c1773f9d94d01fb66b96651672
Gilles ORSINI <gilles.orsini@gmail.com>
2022-02-13 09:58:49 +0100

    add PersistPhotoChoose UT

---
+++ lib-photos/src/test/java/gnn/com/photos/sync/PersistChooseTest.java
@@ -0,0 +1,31 @@
+package gnn.com.photos.sync;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.core.Is;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+import java.util.Date;
+
+import gnn.com.photos.Photo;
+
+public class PersistChooseTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void name() throws IOException {
+        // when
+        PersistChoose persist = new PersistChoose(temporaryFolder.newFolder());
+        Photo photo = new Photo("url", "id");
+        Date date = new Date(74, 3-1, 9, 12, 30);
+        persist.write(photo);
+        PhotoChoose photoResult = persist.read();
+        assertThat(photoResult.photo.getId(), Is.is(photo.getId()));
+    }
+}

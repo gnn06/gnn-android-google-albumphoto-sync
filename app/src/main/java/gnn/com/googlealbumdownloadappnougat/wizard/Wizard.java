6112d5a357bc059eaf77f9b9f2f5a7c9096b479a
Gilles ORSINI <gilles.orsini@gmail.com>
2022-04-17 10:22:55 +0200

    Wizard.getNextStep with UT

---
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/Wizard.java
@@ -0,0 +1,46 @@
+package gnn.com.googlealbumdownloadappnougat.wizard;
+
+import gnn.com.googlealbumdownloadappnougat.auth.AuthManager;
+import gnn.com.googlealbumdownloadappnougat.ui.presenter.PersistPrefMain;
+import gnn.com.googlealbumdownloadappnougat.wallpaper.MyWallpaperService;
+import gnn.com.googlealbumdownloadappnougat.wallpaper.WallpaperScheduler;
+
+public class Wizard {
+
+    final private AuthManager authManager;
+    private final PersistPrefMain persistPrefMain;
+    private final MyWallpaperService wallpaperService;
+    private final WallpaperScheduler wallpaperScheduler;
+
+    public Wizard(AuthManager authManager, PersistPrefMain persistPrefMain, MyWallpaperService wallpaperService, WallpaperScheduler wallpaperScheduler) {
+        this.authManager = authManager;
+        this.persistPrefMain = persistPrefMain;
+        this.wallpaperService = wallpaperService;
+        this.wallpaperScheduler = wallpaperScheduler;
+    }
+
+    public WizardStep getNextStep() {
+        if (!authManager.isSignIn()) {
+            return WizardStep.S01_LOGIN;
+        } else if (!authManager.hasGooglePermission()) {
+            return WizardStep.S02_ASK_GOOGLE_PERMISSION;
+        } else if (persistPrefMain.getAlbum() == null) {
+            return WizardStep.S03_CHOOSE_ALBUM;
+        } else if (!authManager.hasWritePermission()) {
+            return WizardStep.S04_ASK_WRITE_PERMISSION;
+        } else if (persistPrefMain.getPhotoPath() == null) {
+            return WizardStep.S05_CHOOSE_FOLDER;
+        } else if (!wallpaperService.isActive()) {
+            return WizardStep.S06_ACTIVATE_LIVEWALLPAPER;
+        } else if (persistPrefMain.getFrequencyWallpaper() == PersistPrefMain.DEF_FREQ_WALLPAPER_MINUTE) {
+            return WizardStep.S07_CHOOSE_WALLPAPER_FREQUENCY;
+        } else if (persistPrefMain.getFrequencyDownload() == PersistPrefMain.DEF_FREQ_SYNC_HOUR) {
+            return WizardStep.S08_CHOOSE_DOWNLOAD_FREQUENCY;
+        } else if (persistPrefMain.getFrequencyUpdatePhotosHour() == PersistPrefMain.DEF_FREQ_UPDATE_PHOTO_DAY) {
+            return WizardStep.S09_CHOOSE_UPDATE_FREQUENCY;
+        } else if (!wallpaperScheduler.isScheduled()) {
+            return WizardStep.S10_ACTIVATE_SCHEDULER;
+        } else
+            return WizardStep.S11_FINISHED;
+    }
+}
---
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/WizardStep.java
@@ -0,0 +1,17 @@
+package gnn.com.googlealbumdownloadappnougat.wizard;
+
+public enum WizardStep {
+    S01_LOGIN,
+    S02_ASK_GOOGLE_PERMISSION,
+    S03_CHOOSE_ALBUM,
+    S04_ASK_WRITE_PERMISSION,
+    S05_CHOOSE_FOLDER,
+    // éventuellement choisir le nombre de photo à synchroniser)
+    // (faire un test de synchro)
+    S06_ACTIVATE_LIVEWALLPAPER,
+    S07_CHOOSE_WALLPAPER_FREQUENCY,
+    S08_CHOOSE_DOWNLOAD_FREQUENCY,
+    S09_CHOOSE_UPDATE_FREQUENCY,
+    S10_ACTIVATE_SCHEDULER,
+    S11_FINISHED
+}

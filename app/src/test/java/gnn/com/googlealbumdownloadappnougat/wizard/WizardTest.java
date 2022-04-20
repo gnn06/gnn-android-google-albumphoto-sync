469339ac320c4ad79f3bb3d1b1ab2cc063c987ac
Gilles ORSINI <gilles.orsini@gmail.com>
2022-04-20 21:48:57 +0200

    refactor UT using robolectric

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/wizard/WizardTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/wizard/WizardTest.java
@@ -5,28 +5,37 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import gnn.com.googlealbumdownloadappnougat.auth.AuthManager;
 import gnn.com.googlealbumdownloadappnougat.ui.presenter.PersistPrefMain;
 import gnn.com.googlealbumdownloadappnougat.wallpaper.MyWallpaperService;
 import gnn.com.googlealbumdownloadappnougat.wallpaper.WallpaperScheduler;
+import androidx.test.core.app.ApplicationProvider;
 
+@RunWith(RobolectricTestRunner.class)
 public class WizardTest {
 
     private AuthManager authManager;
     private PersistPrefMain persistPrefMain;
     private MyWallpaperService wallpaperService;
     private WallpaperScheduler wallpaperScheduler;
+    private SharedPreferences preferences;
 
     @Before
     public void setUp() throws Exception {
-        this.authManager = mock(AuthManager.class);
-        this.persistPrefMain = mock(PersistPrefMain.class);
-        this.wallpaperService = mock(MyWallpaperService.class);
-        this.wallpaperScheduler = mock(WallpaperScheduler.class);
+        this.authManager = null;
+        this.persistPrefMain = new PersistPrefMain(ApplicationProvider.getApplicationContext());
+        this.wallpaperService = null;
+        this.wallpaperScheduler = null;
+        preferences = PreferenceManager.getDefaultSharedPreferences(ApplicationProvider.getApplicationContext());
     }
 
     @Test
@@ -36,14 +45,17 @@ public class WizardTest {
         // when
         wizard.setStep(WizardStep.S02_ASK_GOOGLE_PERMISSION);
         // then
-        verify(persistPrefMain).saveWizardStep(WizardStep.S02_ASK_GOOGLE_PERMISSION);
+        String value = preferences.getString("wizard_active", "AZE");
+        assertThat(value, Is.is(WizardStep.S02_ASK_GOOGLE_PERMISSION.name()));
     }
 
     @Test
     public void persist_restore_step() {
         // Given
         Wizard wizard = new Wizard(this.authManager, persistPrefMain, wallpaperService, wallpaperScheduler);
-        when(persistPrefMain.restoreWizardStep()).thenReturn(WizardStep.S03_CHOOSE_ALBUM);
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putString("wizard_active", WizardStep.S03_CHOOSE_ALBUM.name());
+        edit.apply();
         // when
         WizardStep step = wizard.getStep();
         // then

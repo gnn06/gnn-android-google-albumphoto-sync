babdb13587e175ac6cddcedcbeb4b076459ac6f7
Gilles ORSINI <gilles.orsini@gmail.com>
2022-05-08 08:47:01 +0200

    WIP inflate wizard fragment

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/PresenterWizard.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/PresenterWizard.java
@@ -1,7 +1,9 @@
 package gnn.com.googlealbumdownloadappnougat.wizard;
 
-import android.app.Activity;
+import android.view.View;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -11,10 +13,10 @@ import gnn.com.googlealbumdownloadappnougat.ui.presenter.PersistPrefMain;
 public class PresenterWizard {
 
     final private FragmentWizard view;
-    final private Activity activity;
+    final private FragmentActivity activity;
     private final PersistPrefMain persist;
 
-    public PresenterWizard(Activity activity, FragmentWizard view) {
+    public PresenterWizard(FragmentActivity activity, FragmentWizard view) {
         this.view = view;
         this.activity = activity;
         persist = new PersistPrefMain(this.activity);
@@ -26,8 +28,11 @@ public class PresenterWizard {
     }
 
     public void switchToWizard() {
-        NavController controller = Navigation.findNavController(this.activity, R.id.fragment_container_view);
-        controller.navigate(R.id.action_fragmentHome_to_fragmentWizard);
+//        NavController controller = Navigation.findNavController(this.activity, R.id.fragment_container_view);
+//        controller.navigate(R.id.action_fragmentHome_to_fragmentWizard);
+        FragmentTransaction transaction = this.activity.getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container_wizard, FragmentWizard.class, null);
+        transaction.commit();
     }
 
     public void nextStep() {

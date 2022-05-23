4aaae639993e72ac324e97d4925b7ba114157f8b
Gilles ORSINI <gilles.orsini@gmail.com>
2022-05-23 08:49:07 +0200

    fix setExplanation when ste = finished

--- app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/FragmentWizard.java
+++ app/src/main/java/gnn/com/googlealbumdownloadappnougat/wizard/FragmentWizard.java
@@ -44,18 +44,20 @@ public class FragmentWizard extends Fragment {
         });
     }
 
-    public void setExplanation(WizardStep step) {
-        int index = step.ordinal();
-        if (index == WizardStep.values().length - 1) {
+    public void setExplanation(WizardStep stepDone) {
+        int index = stepDone.ordinal(); // 0 when step = S00_NOT_STARTED;
+        if (stepDone == WizardStep.S11_FINISHED) {
+            // if wizard is finished, use before last step
             index = WizardStep.values().length - 2;
         }
         TextView stepView = getView().findViewById(R.id.text_wizard_step);
         String stringStep = getActivity().getResources().getString(R.string.text_wizard_step, index + 1, WizardStep.values().length - 1);
         stepView.setText(stringStep);
 
-        String[] strings = getActivity().getResources().getStringArray(R.array.wizard_explaination);
+        String[] strings = getActivity().getResources().getStringArray(R.array.wizard_explanation);
         TextView view = getView().findViewById(R.id.text_wizard_explaination);
-        view.setText(strings[index]);
+        String explanation = strings[index];
+        view.setText(explanation);
     }
 
     @Override

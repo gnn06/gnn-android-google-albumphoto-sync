924c6f07381e99e8b8d16095598bbf16f70b2b92
Gilles ORSINI <gilles.orsini@gmail.com>
2022-06-24 16:05:55 +0200

    add espresso but was already there

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/FragmentFrequenciesIntTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/FragmentFrequenciesIntTest.java
@@ -1,5 +1,8 @@
 package gnn.com.googlealbumdownloadappnougat.ui.presenter;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
@@ -8,11 +11,14 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.espresso.matcher.ViewMatchers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import gnn.com.googlealbumdownloadappnougat.R;
 import gnn.com.googlealbumdownloadappnougat.ServiceLocator;
 import gnn.com.googlealbumdownloadappnougat.wallpaper.WallpaperScheduler;
 
@@ -40,5 +46,23 @@ public class FragmentFrequenciesIntTest {
         
         // in test, getActivity=FragmentActivity
         // in app, getActivity=Mainactivity
+    }
+
+    @Test
+    public void toggle_with_no_permission() {
+        WallpaperScheduler schedulerMock = mock(WallpaperScheduler.class);
+        when(schedulerMock.isScheduled()).thenReturn(true);
+        ServiceLocator.getInstance().setWallpaperScheduler(schedulerMock);
+
+        WallpaperSchedulerWithPermission taskMock = mock(WallpaperSchedulerWithPermission.class);
+        ServiceLocator.getInstance().setSyncTask(taskMock);
+
+        FragmentScenario<FragmentFrequencies> scenario = FragmentScenario.launchInContainer(FragmentFrequencies.class);
+
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        onView(withId(R.id.SwitchWallPaper)).perform();
+
+        verify(taskMock, never()).schedule(anyLong(), anyInt(), anyLong());
     }
 }

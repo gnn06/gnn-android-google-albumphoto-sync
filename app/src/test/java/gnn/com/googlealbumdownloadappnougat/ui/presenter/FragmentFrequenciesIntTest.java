47821620646fb1bbac4aae1575eeb5848bec8dfe
Gilles ORSINI <gilles.orsini@gmail.com>
2022-06-24 04:32:42 +0200

    make robolectric works with mockito

--- app/src/test/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/FragmentFrequenciesIntTest.java
+++ app/src/test/java/gnn/com/googlealbumdownloadappnougat/ui/presenter/FragmentFrequenciesIntTest.java
@@ -1,10 +1,18 @@
 package gnn.com.googlealbumdownloadappnougat.ui.presenter;
 
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import androidx.fragment.app.testing.FragmentScenario;
-import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.RobolectricTestRunner;
 
 import gnn.com.googlealbumdownloadappnougat.ServiceLocator;
@@ -13,27 +21,24 @@ import gnn.com.googlealbumdownloadappnougat.wallpaper.WallpaperScheduler;
 @RunWith(RobolectricTestRunner.class)
 public class FragmentFrequenciesIntTest {
 
+//    @Rule
+//    public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
     @Test
     public void test() {
         // Check Fragment with Presenter
         // mock schedule
-        WallpaperScheduler schedulerMock = new WallpaperScheduler(ApplicationProvider.getApplicationContext()) {
-            @Override
-            public boolean isScheduled() {
-                return true;
-            }
-        };
+
+        WallpaperScheduler schedulerMock = mock(WallpaperScheduler.class);
+        when(schedulerMock.isScheduled()).thenReturn(true);
         ServiceLocator.getInstance().setWallpaperScheduler(schedulerMock);
 
-        ScheduleTask taskMock = new ScheduleTask(null, null, null, null, null) {
-            @Override
-            void schedule(boolean checked, long frequencyWallpaperMinute, int frequencySyncMinute, long frequencyUpdatePhotosHour) {
-                System.out.println("gnn");
-            }
-        };
+        ScheduleTask taskMock = mock(ScheduleTask.class);
         ServiceLocator.getInstance().setSyncTask(taskMock);
 
         FragmentScenario<FragmentFrequencies> scenario = FragmentScenario.launch(FragmentFrequencies.class);
+
+//        verify(taskMock, never()).schedule(anyBoolean(), anyLong(), anyInt(), anyLong());
         org.junit.Assert.assertTrue(true);
         // in test, getActivity=FragmentActivity
         // in app, getActivity=Mainactivity

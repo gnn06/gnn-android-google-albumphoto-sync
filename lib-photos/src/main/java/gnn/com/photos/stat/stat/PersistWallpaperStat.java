f82b833821feca557ab63334fb79074c7f0806d2
Gilles ORSINI <gilles.orsini@gmail.com>
2022-12-26 08:53:25 +0100

    fix : dump stat json file ito n log when an exception occurs

--- lib-photos/src/main/java/gnn/com/photos/stat/stat/PersistWallpaperStat.java
+++ lib-photos/src/main/java/gnn/com/photos/stat/stat/PersistWallpaperStat.java
@@ -2,12 +2,15 @@ package gnn.com.photos.stat.stat;
 
 import com.google.gson.Gson;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 
 import gnn.com.googlealbumdownloadappnougat.util.Logger;
 
@@ -37,7 +40,12 @@ class PersistWallpaperStat {
             WallpaperStat result = new Gson().fromJson(reader, WallpaperStat.class);
             if (result == null) {
                 Logger logger = Logger.getLogger();
-                logger.severe("can't read wallpaper stat. Gson returns null. processFolder=" + processFolder + ", filename=" + STAT_FILENAME);
+                try {
+                    logger.severe("can't read wallpaper stat. Gson returns null. processFolder=" + processFolder + ", filename=" + STAT_FILENAME + "(file=" +
+                                    FileUtils.readFileToString(new File(processFolder, PersistWallpaperStat.STAT_FILENAME), "UTF-8") + ")");
+                } catch (IOException e) {
+                    logger.severe("can't read wallpaper stat. Gson returns null. can't read file " + processFolder + ", filename=" + STAT_FILENAME);
+                }
                 throw new NullPointerException("can't read wallpaper stat. processFolder=" + processFolder + ", filename=" + STAT_FILENAME);
             }
             if (result.getDate() == null) {
